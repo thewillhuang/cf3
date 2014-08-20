921997b6cf3252aeb401fbac8639655fdd85a2de
William Huang <will.h86@gmail.com>
2014-08-20 01:05:38 -0700

    some update

--- js/views/library.js
+++ js/views/library.js
@@ -3,9 +3,13 @@ var app = app || {};
 app.LibraryView = Backbone.View.extend({
     el: '#books',
 
-    initialize: function( initialBooks ) {
-        this.collection = new app.Library( initialBooks );
+    initialize: function() {
+        this.collection = new app.Library();
+        this.collection.fetch({reset: true}); // NEW
         this.render();
+
+        this.listenTo( this.collection, 'add', this.renderBook );
+        this.listenTo( this.collection, 'reset', this.render ); // NEW
     },
 
     // render library by rendering each book in its collection
